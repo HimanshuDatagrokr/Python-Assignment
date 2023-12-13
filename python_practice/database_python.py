a413339ece8e69df9e43f3af6d87644287c56b6c
himanshu <himanshu.prasad@datagrokr.com>
2023-12-13 12:05:16 +0530

    completed python asssigment part 1

--- python_practice/database_python.py
+++ python_practice/database_python.py
@@ -1,30 +1,39 @@
 from mysql.connector import connect, Error
 
 
-try:
-    conn = connect(
-    user = 'root',
-    password ='himanshu',
-    host = 'localhost',
-    database = 'datagrokr')
-    
-    sql_select_Query = "select * from user_details"
-    cursor = conn.cursor()
-    cursor.execute(sql_select_Query)
-    records = cursor.fetchall()
-    print("Total number of rows in table:",cursor.rowcount)
-    
-    print("\nPrinting each row")
-    for row in records:
-        print("Id = ", row[0])
-        print("Name = ", row[1])
-        print("Price = ", row[2])
-        print("Purchase date =",row[3],"\n")
+def fetch_data_generator():
+    try:
+        conn = connect(
+            user='root',
+            password='himanshu',
+            host='localhost',
+            database='datagrokr'
+        )
 
-except Error as e:
-    print("Error reading data from MySQL table", e)
-finally:
-    if conn.is_connected():
-        conn.close()
-        cursor.close()
-        print("Mysql connection is closed")
+        sql_select_Query = "select * from user_details"
+        cursor = conn.cursor()
+        cursor.execute(sql_select_Query)
+
+        while True:
+            # Fetch one row at a time
+            row = cursor.fetchone()
+
+            if not row:
+                break
+
+            yield row
+
+    except Error as e:
+        print("Error reading data from MySQL table", e)
+    finally:
+        if conn.is_connected():
+            conn.close()
+            cursor.close()
+            print("MySQL connection is closed")
+
+# Using the generator in a loop
+for row in fetch_data_generator():
+    print("Id =", row[0])
+    print("Name =", row[1])
+    print("Price =", row[2])
+    print("Purchase date =", row[3], "\n")
